e358f0bf07b6b8c149d3392cfd476693e8d2cb96
Okocha Ebube <okochaebube@gmail.com>
2019-08-24 12:07:20 +0100

    Done with the login and signup network requests

--- app/src/main/java/com/gigabytedevs/apps/midclan/activities/SignInActivity.java
+++ app/src/main/java/com/gigabytedevs/apps/midclan/activities/SignInActivity.java
@@ -2,19 +2,31 @@ package com.gigabytedevs.apps.midclan.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatTextView;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.balysv.materialripple.MaterialRippleLayout;
 import com.gigabytedevs.apps.midclan.R;
+import com.gigabytedevs.apps.midclan.utils.TinyDb;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.UnsupportedEncodingException;
 
 public class SignInActivity extends AppCompatActivity {
 
     private AppCompatEditText username, password;
     private MaterialRippleLayout facebook, twitter, signIn;
     private AppCompatTextView createAccount, forgotPassword;
+    String mRequestBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +55,66 @@ public class SignInActivity extends AppCompatActivity {
         twitter.setOnClickListener(view -> {
 
         });
+
+        signIn.setOnClickListener(view -> {
+            sendSignInRequest();
+        });
+    }
+
+    private void sendSignInRequest(){
+        String base_url_signin = getResources().getString(R.string.base_url_sign_in) + "user";
+        JSONObject params = new JSONObject();
+
+        //Creating the body of the request
+        try {
+            params.put("email", String.valueOf(username.getText()).toLowerCase());
+            params.put("password", String.valueOf(password.getText()).toLowerCase());
+            mRequestBody = params.toString();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Making the login request using volley library
+        JsonObjectRequest signIn = new JsonObjectRequest(Request.Method.POST, base_url_signin, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //the response gotten when the request has been sent
+                Toast.makeText(SignInActivity.this, "User Logged In", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Getting the error body from the server
+                try {
+                    String responseBody = new String(error.networkResponse.data, "utf-8");
+                    JSONObject jsonObject = new JSONObject(responseBody);
+
+                    //Getting the message from the json
+                    String message = jsonObject.getJSONObject("error").getString("message");
+
+                    Toast.makeText(SignInActivity.this, message, Toast.LENGTH_LONG).show();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }){
+            @Override
+            public byte[] getBody() {
+                //Sending the user parameters as a body to the server
+
+                try {
+                    return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(signIn);
     }
 }
--- app/src/main/java/com/gigabytedevs/apps/midclan/service/PatientClient.java
+++ app/src/main/java/com/gigabytedevs/apps/midclan/service/PatientClient.java
@@ -4,9 +4,11 @@ import com.gigabytedevs.apps.midclan.models.api_models.PatientModel;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface PatientClient {
+    @Headers("Content-Type: application/json")
     @POST("user")
-    Call<PatientModel> createAccount(@Body PatientModel patientModel);
+    Call<PatientModel> createAccount(@Body String body);
 }
